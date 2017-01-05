969f47e37e4c6329b598056045075fecf03dabe0
Sidharth Agarwal <syd@live.in>
2017-01-05 20:38:14 +0000

    raw leaderboard code

--- appengine-game/main.py
+++ appengine-game/main.py
@@ -22,7 +22,7 @@ import json
 from google.appengine.api import urlfetch
 from google.appengine.api import users
 from google.appengine.ext import ndb
-from django.utils import simplejson as json
+#from django.utils import simplejson as json
 
 import races
 import models
@@ -41,7 +41,7 @@ class Generate(webapp2.RequestHandler):
 
     def get(self):
 
-        for i in range(1, 100):
+        for i in range(30, 130):
             result = urlfetch.fetch('http://www.seanwrona.com/typeracer/text.php?id=' + str(i))
             htmltree = lxml.html.fromstring(result.content)
 
@@ -51,7 +51,35 @@ class Generate(webapp2.RequestHandler):
             quote = p_content[0]
             source = p_content[1]
 
-            print(str(i) + ' "' + quote + '" "' + source + '"');
+            print(str(i) + ' "' + quote + '" "' + source + '"')
+            print(i);
+
+            tempEx = models.Excerpt(passage = quote, source = source)
+            tempEx.key = ndb.Key(models.Excerpt, i-30);
+            tempEx.put()
+
+class Load(webapp2.RequestHandler):
+    def get(self):
+        for i in range(0,100):
+            tempEx = ndb.Key(models.Excerpt, i).get()
+            print(str(i) + ' "' + tempEx.passage + '" "' + tempEx.source + '"');
+
+# [START Leaderboard]
+class Leaderboard(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        responseDict = dict()
+        if user:
+            query = Player.query(Player.user_id == user.user_id())
+            player = query.fetch(1);
+            responseDict["u"] = json.dumps(player[0].to_dict())
+        else:
+
+        PLAYERS_PER_PAGE = 10
+        query = Player.query().order(-Player.wpm)
+        leaders = query.fetch(PLAYERS_PER_PAGE)
+        responseDict["lb"] = json.dumps(leaders.to_dict())
+        self.response.write(responseDict)
 
 # [START main_page]
 class MainPage(webapp2.RequestHandler):
@@ -111,6 +139,15 @@ class Play(webapp2.RequestHandler):
 
     def save(self):
         raceStats.put()
+        CUR_PLAYER_WPM = 10; #TODO: get from game
+        query = Player.query(Player.user_id == user.user_id())
+        qPlayers = query.fetch(1)
+        player = qPlayers[0]
+        oldWPM = player.wpm
+        newWPM = ((oldWPM*player.games_played)+CUR_PLAYER_WPM)/(player.games_played+1)
+        player.wpm = newWPM
+        player.put()
+
     def getRoomKey(self):
         race_key = race.put()
     def postRoom(self):
@@ -118,7 +155,7 @@ class Play(webapp2.RequestHandler):
         "race_id" : race_key,
         "startTime" : race.created_at,
         "text" : race.text
-        
+
         }
 
 # [END play]
@@ -128,6 +165,8 @@ app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/play', Play),
     ('/generate', Generate),
+    ('/load', Load),
     ('/races/new', 'races.New'),
+    ('/leaderboard', Leaderboard)
 ], debug=True)
 # [END app]
