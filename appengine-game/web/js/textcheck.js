3f40f129de1ff33074003b2c2f4781f3d545b707
Benoit Demaegdt <benoit.demaegdt@phelma.grenoble-inp.fr>
2017-01-07 18:53:56 +0100

    correct word color bug + refactor code

--- appengine-game/web/js/textcheck.js
+++ appengine-game/web/js/textcheck.js
@@ -1,56 +1,86 @@
-// Asynchronous request to get the data
-// $.getJSON("data/data.json", function(json) {
+/**
+ * check matching between user input and text
+ * @param string   text to be typed
+ */
 function textCheck(receivedText) {
-  //generate text and input
+
+  // plot text
+  displayParagraph(receivedText)
+
   var myParagraph = document.getElementById("paragraph-input")
-  // myParagraph.innerHTML = json.text
-  myParagraph.innerHTML = receivedText;
-
-  // get the value of the paragraph and split it
-  var myText = document.getElementById("paragraph-input").innerHTML
-  var myArray = myText.split(" ")
-
+  var myArray = myParagraph.innerHTML.split(" ")
   // input
   var myInput = document.getElementById("user-input")
 
+  handleWordColor(0, true)
 
-  var re1 = new RegExp(myArray[0],"g")
-
-  $("p:contains(" + myArray[0] + ")").html(function(_, html) {
-    return html.replace(re1, '<span>'+ myArray[0] + '</span>')
-  })
-
-  // handle the event
+  // handle events
   var count = 0
   myInput.onkeypress = function(evt) {
     evt = evt || window.event
     if (evt.keyCode == 32) {
-      if(count >= myArray.length) {
+      if(count >= myArray.length-1) {
+        myParagraph.innerHTML = myParagraph.innerHTML.replace(/<\/?span[^>]*>/g,"")
+        myInput.value = ""
         return false
       }
       var myWord = myInput.value
       if (myWord == myArray[count]) {
-        myInput.style.backgroundColor = "white";
-        // change color of words
-        var myText2 = document.getElementById("paragraph-input")
-        myText2.innerHTML = myText2.innerHTML.replace(/<\/?span[^>]*>/g,"")
-        var re3 = new RegExp(myArray[count+1],"g")
-        $("p:contains(" + myArray[count+1] + ")").html(function(_, html) {
-          return html.replace(re3, '<span>'+ myArray[count+1] + '</span>')
-        })
+        changeInputColor("white")
+        handleWordColor(count+1, true)
         count += 1
         // reset value of the input
         myInput.value = ""
         return false
       } else {
-        myInput.style.backgroundColor = "red";
-        var re3 = new RegExp(myArray[count],"g")
-        $("p:contains(" + myArray[count] + ")").html(function(_, html) {
-          return html.replace(re3, '<span class="wrong">'+ myArray[count] + '</span>')
-        })
+        changeInputColor("red")
+        handleWordColor(count, false)
         return false
       }
     }
   }
 }
-// })
+
+/**
+ * Display the text to be typed
+ * @param string   text to be typed
+ */
+ function displayParagraph(receivedText) {
+  
+  var myParagraph = document.getElementById("paragraph-input")
+  myParagraph.innerHTML = receivedText
+
+}
+
+/**
+ * Change color of a word
+ * @param integer   index of the word in the string
+ * @param boolean   span if the word is correct, class='wrong' otherwise 
+ */
+function handleWordColor(index, isCorrect) {
+
+  var myParagraph = document.getElementById("paragraph-input")
+  myParagraph.innerHTML = myParagraph.innerHTML.replace(/<\/?span[^>]*>/g,"")
+
+  var myArray = myParagraph.innerHTML.split(" ")
+
+  if(isCorrect === true) {
+    myArray[index] = "<span>" + myArray[index] + "</span>"  
+  } else if(isCorrect === false) {
+    myArray[index] = "<span class='wrong'>" + myArray[index] + "</span>"
+  }
+  
+  myParagraph.innerHTML = myArray.join(" ")
+
+}
+
+/**
+ * Change background color of the input box
+ * @param string   background color
+ */
+function changeInputColor(color) {
+
+  var myInput = document.getElementById("user-input")
+  myInput.style.backgroundColor = color
+
+}
