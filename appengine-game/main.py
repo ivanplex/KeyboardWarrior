0a86e19884e4c6ef9ebccdb16743de66ffbe8729
Kheng Boon Pek <boonpek96@gmail.com>
2017-01-08 14:20:33 +0000

    fixed else if -> elif

--- appengine-game/main.py
+++ appengine-game/main.py
@@ -411,7 +411,7 @@ class Play(webapp2.RequestHandler):
                             player['mistakes'] = mistakes
 
                     # we want to keep player ping even though game hasn't started
-                    else if room['start_time'] == -1:
+                    elif room['start_time'] == -1:
                         player['updated_at'] = current_time
 
         else:
